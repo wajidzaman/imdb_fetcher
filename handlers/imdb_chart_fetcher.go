e10f8cacf086dbb48f597d52e427d6f65ff7a3a3
MOHD. WAJID ZAMAN <mohd.w.zaman@getfitso.com>
2020-09-28 00:01:19 +0530

    update pkg

--- handlers/imdb_chart_fetcher.go
+++ handlers/imdb_chart_fetcher.go
@@ -1,8 +1,12 @@
 package handlers
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strings"
+
+	"golang.org/x/net/html"
 )
 
 type ImdbChartFetcher struct {
@@ -24,9 +28,67 @@ func (i *ImdbChartFetcher) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 	// if no method is satisfied return an error
 	rw.WriteHeader(http.StatusMethodNotAllowed)
 }
+func (i *ImdbChartFetcher) getHref(t html.Token) (ok bool, href string) {
+	// Iterate over token attributes until we find an "href"
+	for _, a := range t.Attr {
+		if a.Key == "href" && strings.Contains(a.Val, "title") {
+			href = a.Val
+			ok = true
+		}
+	}
+
+	return
+}
 func (i *ImdbChartFetcher) fetchImdbChart(rw http.ResponseWriter, r *http.Request) {
 	i.l.Println("Handle GET Products")
 
+	url := "https://www.imdb.com/india/top-rated-indian-movies/"
+	resp, err := http.Get(url)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	fmt.Println("Response status:", resp.Status)
+
+	//scanner := bufio.NewScanner(resp.Body)
+	b := resp.Body
+	defer b.Close() // close Body when the function completes
+
+	z := html.NewTokenizer(b)
+
+	for {
+		tt := z.Next()
+		//	b := z.Text()
+		//fmt.Println("ttt----", tt.Data)
+		switch {
+		case tt == html.ErrorToken:
+			// End of the document, we're done
+			return
+		}
+		t := z.Token()
+
+		// Check if the token is an <a> tag
+
+		isAnchor := t.Data == "a"
+
+		if !isAnchor {
+			continue
+		}
+
+		// Extract the href value, if there is one
+		ok, url := i.getHref(t)
+		if !ok {
+
+			continue
+		}
+
+		// Make sure the url begines in http**
+		hasProto := strings.Index(url, "http") == 0
+
+		fmt.Println("link------", url, hasProto)
+
+	}
 	// fetch the products from the datastore
 	/*
 		lp := data.GetProducts()
