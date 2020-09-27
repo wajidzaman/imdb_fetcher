c9766b34d9fa7eb61e981090c8b66cc74c55b634
MOHD. WAJID ZAMAN <mohd.w.zaman@getfitso.com>
2020-09-27 22:55:45 +0530

    pkg updated

--- handlers/imdb_chart_fetcher.go
+++ handlers/imdb_chart_fetcher.go
@@ -9,18 +9,20 @@ type ImdbChartFetcher struct {
 	l *log.Logger
 }
 
-func ImdbChartFetcher(l *log.Logger) *ImdbChartFetcher {
+func NewImdbChartFetcher(l *log.Logger) *ImdbChartFetcher {
 	return &ImdbChartFetcher{l}
 }
 func (i *ImdbChartFetcher) fetchImdbChart(rw http.ResponseWriter, r *http.Request) {
 	i.l.Println("Handle GET Products")
 
 	// fetch the products from the datastore
-	lp := data.GetProducts()
+	/*
+		lp := data.GetProducts()
 
-	// serialize the list to JSON
-	err := lp.ToJSON(rw)
-	if err != nil {
-		http.Error(rw, "Unable to marshal json", http.StatusInternalServerError)
-	}
+		// serialize the list to JSON
+		err := lp.ToJSON(rw)
+		if err != nil {
+			http.Error(rw, "Unable to marshal json", http.StatusInternalServerError)
+		}
+	*/
 }
