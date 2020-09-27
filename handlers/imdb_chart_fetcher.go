d9ebdfc4855d92733c70b95481b5b7fe8951cff1
MOHD. WAJID ZAMAN <mohd.w.zaman@getfitso.com>
2020-09-27 22:27:33 +0530

    initial commit

---
+++ handlers/imdb_chart_fetcher.go
@@ -0,0 +1,26 @@
+package handlers
+
+import (
+	"log"
+	"net/http"
+)
+
+type ImdbChartFetcher struct {
+	l *log.Logger
+}
+
+func ImdbChartFetcher(l *log.Logger) *ImdbChartFetcher {
+	return &ImdbChartFetcher{l}
+}
+func (i *ImdbChartFetcher) fetchImdbChart(rw http.ResponseWriter, r *http.Request) {
+	i.l.Println("Handle GET Products")
+
+	// fetch the products from the datastore
+	lp := data.GetProducts()
+
+	// serialize the list to JSON
+	err := lp.ToJSON(rw)
+	if err != nil {
+		http.Error(rw, "Unable to marshal json", http.StatusInternalServerError)
+	}
+}
